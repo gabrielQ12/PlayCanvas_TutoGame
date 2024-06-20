64761228875a3aa77fbba4d9079ecff9b516ba24
gabrielq12 <gabriel.quinette1@gmail.com>
2024-06-20 12:38:40 +0200

    project setup : Done , map  import : DONE

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const canvas = document.querySelector('canvas');
+
+const context = canvas.getContext('2d');
+
+canvas.width = 1024
+canvas.height = 576
+
+context.fillStyle = 'white';
+context.fillRect(0, 0, canvas.width, canvas.height);
+
+console.log('Hello, world!');
+console.log(canvas);
+console.log(context);
