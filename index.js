0ddf6f20ce49daf028e1ceaab17dd525de3a654a
gabrielq12 <gabriel.quinette1@gmail.com>
2024-06-20 13:51:01 +0200

    setup map : DONE

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const canvas = document.querySelector('canvas');
 
 const context = canvas.getContext('2d');
 
-canvas.width = 1024
-canvas.height = 576
+canvas.width = 1024;
+canvas.height = 576;
 
 context.fillStyle = 'white';
 context.fillRect(0, 0, canvas.width, canvas.height);
 
-console.log('Hello, world!');
-console.log(canvas);
-console.log(context);
+const image = new Image();
+image.src = './img/Bourg blabla.png';
+
+
+image.onload = () => {
+    context.drawImage(image,-1000, -300);
+    }
+
+
