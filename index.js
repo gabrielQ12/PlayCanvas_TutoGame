654048fef08574a6badaac971634c6350637508a
gabrielq12 <gabriel.quinette1@gmail.com>
2024-06-26 12:36:48 +0200

    foreground : DONE

--- index.js
+++ index.js
@@ -15,21 +15,6 @@ for (let i = 0; i < collisions.length; i += 70) {
     collisionsMap.push(collisions.slice(i, 70 + i ));
 }
 
-class Boundary {
-    static width = 48;
-    static height = 48;
-    constructor({ position, }) {
-        this.position = position,
-        this.width = 48,
-        this.height = 48
-
-    }
-    draw() {
-        context.fillStyle = 'red'
-        context.fillRect(this.position.x, this.position.y, this.width, this.height)
-    }
-}
-
 const boundaries = []
 
 const offset = {
@@ -57,41 +42,13 @@ collisionsMap.forEach((row, i ) => {
 const image = new Image();
 image.src = './img/Pellet Town.png';
 
+const foregroundImage = new Image();
+foregroundImage.src = './img/foregroundObjects.png';
+
 const playerImage = new Image ();
 playerImage.src = './img/playerDown.png';
 
-class Sprite {
-    constructor({
-        position,
-        velocity,
-        image,
-        frames = { max : 1 }
-    }) {
-        this.position = position
-        this.image = image
-        this.frames = frames
 
-        this.image.onload = () => {
-            this.width = this.image.width / this.frames.max
-            this.height = this.image.height
-        }
-        
-    }
-    draw() {
-
-        context.drawImage(
-            this.image,
-            0,
-            0,
-            this.image.width / this.frames.max,
-            this.image.height,
-            this.position.x,
-            this.position.y,
-            this.image.width / this.frames.max,
-            this.image.height
-        )
-    }
-};
 
 
 const player = new Sprite({
@@ -114,6 +71,14 @@ const background = new Sprite({
     image : image
 })
 
+const foreground = new Sprite({
+    position : {
+        x : offset.x,
+        y : offset.y
+    },
+    image : foregroundImage
+})
+
 const keys = {
     z : {
         pressed: false
@@ -132,7 +97,7 @@ const keys = {
 
 
 
-const movables = [background, ...boundaries]
+const movables = [background, ...boundaries, foreground]
 
 function rectangularCollision( { rectangle1, rectangle2 } ) {
     return (
@@ -152,6 +117,7 @@ function animate() {
 });
 
     player.draw()
+    foreground.draw()
 
     let moving = true
 
@@ -166,7 +132,6 @@ function animate() {
                 } }
             })
         ) {
-                console.log('collision')
                 moving = false
                 break
             }
@@ -186,7 +151,6 @@ function animate() {
                 } }
             })
         ) {
-                console.log('collision')
                 moving = false
                 break
             }
@@ -206,7 +170,6 @@ function animate() {
                 } }
             })
         ) {
-                console.log('collision')
                 moving = false
                 break
             }
@@ -226,7 +189,6 @@ function animate() {
                 } }
             })
         ) {
-                console.log('collision')
                 moving = false
                 break
             }
